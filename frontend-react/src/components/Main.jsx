cc49146ed94c41ec40774ff99a3959fc257ef700
Aditi Sarjerao Patil <aditi.patil3@zensar.com>
2025-08-25 09:03:25 +0530

    homepage react

---
+++ frontend-react/src/components/Main.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import Button from './Button'
+
+const Main = () => {
+  return (
+    <>
+        <div className='container'>
+            <div className='p-5 text-center bg-light-dark rounded'>
+                <h1 className='text-light'>Stock Prediction Portal</h1>
+                <p className='text-light lead'>Welcome to Stock Prediction Portal — your intelligent gateway to smarter investing. Our platform leverages cutting-edge machine learning models to forecast stock trends, helping you make data-driven decisions with confidence. Whether you're a seasoned investor or just starting out, explore insights, track performance, and stay ahead of the market with precision.</p>
+                <Button class="btn-outline-info" text="Login"/>
+                {/* <a className='btn btn-info' href="">Login</a> */}
+            </div>
+
+        </div>
+    </>
+  )
+}
+
+export default Main
